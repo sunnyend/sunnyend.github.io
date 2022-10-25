9ec1191d4b290972a17016bb68dc918a552d45f1
sunnyend <112656181+sunnyend@users.noreply.github.com>
2022-10-25 19:19:19 -0300

    Update script.js
    • "Previous" and "Next" button are working fine
    • When typing a pokemon name/number and clicking "next" and "previous" buttons, the pokedex will retrieve the correct pokemon, not the next/previous from the first one.
    • Shiny button fixed

--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@ const pokemonImage = document.querySelector(".pokemon_image");
 const shinyOrNot = document.querySelector(".shiny-version");
 const form = document.querySelector(".form");
 const input = document.querySelector(".input_search");
+const btnAnte = document.querySelector(".btn-ante");
+const btnProx = document.querySelector(".btn-prox");
 
 
-
+let randomPoke = Math.floor(Math.random() * 649) + 1;
+console.log(randomPoke);
 
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(
@@ -18,30 +21,46 @@ const fetchPokemon = async (pokemon) => {
   const data = await APIResponse.json();
   return data;
 };
-let pokemonType = "default"
+let pokemonType = "default";
 
+
+let currentPoke;
 const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
-    console.log(data);
 
     pokemonName.innerHTML = data.name;  
+    currentPoke = data.id;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][`front_${pokemonType}`];
+    
 
 }
+
+renderPokemon(randomPoke);
+
+
 shinyOrNot.addEventListener("click", () => {
-    pokemonType == "default" ? pokemonType = "shiny" : pokemonType = "default";
-    shinyOrNot.innerHTML == "versão shiny" ? shinyOrNot.innerHTML = "versão padrão"  : shinyOrNot.innerHTML = "versão shiny"
-    renderPokemon(input.value);
- 
+    pokemonType == "default" ? pokemonType = "shiny" :   pokemonType = "default";
+    shinyOrNot.innerHTML == "versão shiny" ? shinyOrNot.innerHTML = "versão padrão"  : shinyOrNot.innerHTML = "versão shiny";
+    input.value == "" ? renderPokemon(currentPoke) : renderPokemon(input.value);
     
 })
 
 form.addEventListener("submit",(event) => {
-
+  
   event.preventDefault();
   renderPokemon(input.value);
 
 })
 
+btnAnte.addEventListener("click", () => {
+  
+  input.value == "" ? renderPokemon(randomPoke -= 1) : renderPokemon(currentPoke-1);
+  
+})
+
+btnProx.addEventListener("click", () => {
+  input.value == "" ? renderPokemon(randomPoke += 1) : renderPokemon(currentPoke+1);
+  
+})
